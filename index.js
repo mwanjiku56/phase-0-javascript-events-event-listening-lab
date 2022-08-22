b88c4ee874eca8bd1678e8602023da0cefc8445a
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-08-22 16:25:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickEvent);
+
 }
+
+function clickEvent (){
+    alert('I was clicked!');
+
+}
+addingEventListener();
